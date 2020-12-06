5fa7fabc3fb33dffaec09b12f6833c3583d20fc9
andyrm8052 <andy1994>
2020-12-06 17:45:49 -0500

    New Edits
    Added several images and corrections to all files.

--- js/javascript.js
+++ js/javascript.js
@@ -10,4 +10,18 @@ function toggleMenu(){
 	menu.classList.toggle('pushed');
 }
 
-
+function toggle(){
+	var blurElements = Array.from(document.getElementsByClassName('blur')); 
+    blurElements.forEach(element => element.classList.toggle('active'));
+    var popup = document.getElementById('popup');
+	popup.classList.toggle('active');
+	
+	let imgClass = event.target.closest('a').id; // reads the id from the <a> tag
+  Array.from(popup.querySelectorAll('img')).forEach(image => { // loops over all images with forEach
+    if (image.classList.contains(imgClass)){ // if it's the image belonging to the <a> tag that was clicked,
+      image.style.display = 'block' // set display to 'block'
+    } else {
+      image.style.display = 'none' // set display to 'none' on all other images
+    }
+  })
+}
